c07cfce940f6d9a408999569d9a77a1afda1fc84
azerbijon123 <aaron.r.gregory@gmail.com>
2020-07-22 11:52:27 +0100

    Added basic files

---
+++ common/SearchBar.js
@@ -0,0 +1,10 @@
+const SearchBar = props => (
+    <div>
+        This is a search bar
+    </div>
+);
+
+ReactDOM.render(
+        <SearchBar />,
+        document.getElementById('main-container'),
+);
