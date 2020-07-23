d796ae36f4373878ce100878ff6467e5141311b2
Aaron Gregory <aarongregory@aarons-mbp.home>
2020-07-23 07:25:19 +0100

    Changes made on mac

---
+++ personalsite/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+function Todos (props) {
+
+    return (
+        <div>
+            <h1>Todos</h1>
+        </div>
+    );
+}
+
+export default Todos;
