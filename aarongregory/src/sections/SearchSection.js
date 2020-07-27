dc2e2dbea7a32d25ade67e4d0f01e0cb12afdf68
azerbijon123 <aaron.r.gregory@gmail.com>
2020-07-27 21:43:54 +0100

    latest

---
+++ aarongregory/src/sections/SearchSection.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/layout/Header';
+import HomeSearch from './components/HomeSearch';
+
+const SearchSection = (props) => {
+  const [searchItems, setSearchItems] = useState(
+    [
+      { 
+          term: 'gitHub',
+          address: 'https://github.com/azerbijon123'
+      },
+      {
+          term: 'facebook',
+          address: 'https://facebook.com'
+      },
+      {
+          term: 'google',
+          address: 'https://google.com'
+      },
+    ]
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomeSearch searchItems={ searchItems } />
+      </header>
+    </div>
+  );
+}
+
+export default SearchSection;
