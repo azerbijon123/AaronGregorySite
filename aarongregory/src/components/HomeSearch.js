b5d224191bde45274613db34bd8f2ca1c2b8ce60
Aaron Gregory <aarongregory@Aarons-MacBook-Pro.local>
2020-07-27 15:03:39 +0100

    Latest

--- aarongregory/src/components/HomeSearch.js
+++ aarongregory/src/components/HomeSearch.js
@@ -4,29 +4,53 @@ import './style/common/common.css'
 const HomeSearch = (props) => {
 
     const[homeSearchBox, setHomeSearchBox] = useState(React.createRef());
+    const[autoComplete, setAutoComplete] = useState(React.createRef());
+    const[autoCompleteValue, setAutoCompleteValue] = useState("");
 
     useEffect(() => {
         homeSearchBox.current.focus();
     });
 
     const searchTermFromList = (e) => {
-        // for (var searchItem in props.searchItems)
-        // {
-        //     console.log(searchItem);
-        // }
+        if (e.target.value === ""){
+            setAutoCompleteValue("");
+            autoComplete.current.value = "";
+            return;
+        }
+
+            
+        console.log(e.target.value);
+        
         props.searchItems.forEach(item => {
-                    // if(item.term.startsWith(e.target.value.toLowerCase())){
-                    //     console.log(item);
-                    // }
-                    console.log(item.term.startsWith(e.target.value.toLowerCase()));
-                    //console.log(e.target.value.toLowerCase());
-                }
-            );
+                    if (item.term.startsWith(e.target.value.toLowerCase())) {
+                        console.log(item.term);
+                        setAutoCompleteValue(item.term);
+                        autoComplete.current.value = item.term;
+                    }
+                });
+    
+        
+    };
+
+    const onInputKeyPress = (e) => {
+        if (e.key === "Enter") {
+            
+        }
     };
 
     return(
         <div>
-            <input onChange={ searchTermFromList } ref={homeSearchBox} style={ homeSearchStyle } className="homeSearchBox" type="text" placeholder="Welcome..."></input>
+            <input 
+                onChange={ searchTermFromList }
+                onKeyPress={ onInputKeyPress }
+                ref={ homeSearchBox } 
+                style={ homeSearchStyle } 
+                className="homeSearchBox" 
+                type="text" 
+                placeholder="Welcome..." 
+            />
+
+            <input ref={ autoComplete }></input>
         </div>
     );
 }
