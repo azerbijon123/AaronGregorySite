30c46980b560fd2e080468bcfe08ce7f2bd6d47e
Gregory Aaron - IT Development Specialist <gregora@pht-master.xports.nhs.uk>
2020-07-29 21:42:29 +0100

    More stuff

--- aarongregory/src/components/layout/MinestroneFeatureHeader.js
+++ aarongregory/src/components/layout/MinestroneFeatureHeader.js
@@ -3,21 +3,33 @@ import './MinestroneFeatures.css'
 
 const MinestroneFeatureHeader = (props) => {
     return(
-        <header className="header-style">
-            <div className="header-container">
-                <div className="header-left-container">
-                    <div className="minestrone-header-text">Minestrone</div>
-                    <div className="whats-new">What's New</div>
+        <div className="main-container">
+            <header className="header-style header-style-custom">
+                <div className="header-container">
+                    <div className="header-left-container">
+                        <div className="minestrone-header-text">Minestrone |</div>
+                        <div className="whats-new">What's New</div>
+                    </div>
+                    <div className="header-menu-container">
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="header-menu-container">
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
+            </header>
+            
+            <div className="body-main">
+                <div className="whats-new-panel">
+                    What's new
+                </div>
+                <div className="whats-new-details-panel">
+                    Whats new details panel
                 </div>
             </div>
-        </header>
+
+        </div>
     );
 }
 
