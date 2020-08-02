66210cd425815f0f6f40b32b747edc68be5b6d2b
Gregory Aaron - IT Development Specialist <gregora@pht-master.xports.nhs.uk>
2020-08-02 10:13:58 +0100

    Added new Asia section

--- aarongregory/src/components/layout/main/HomeAsia.js
+++ aarongregory/src/components/layout/main/HomeAsia.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import HomeAsiaBackgroundImage from '../../../images/asia.jpg'
+import HomeAsiaBackgroundImage from '../../../images/asiaNewTest.jpg'
+import {Link} from 'react-router-dom';
 
 const HomeAsia = (props) => {
     return(
         <div style={homeAsiaBackground}>
             <div style={imageHolder}>
 
+            </div>
+            <div style={roadToAsia}>
+                <div style={roadToAsiaTitle}>
+                    <h1>The Road to Asia</h1>
+                    <p><Link to="/mf">Click Here</Link> to read about my first trip to Laos.</p>
+                </div>
             </div>
         </div>
     );
@@ -15,13 +22,25 @@ const homeAsiaBackground = {
     height: '100%',
     width: '100%',
     display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: 'red'
-}
+    flexDirection: 'row'
+};
 
 const imageHolder = {
+    flex: '1',
     height: '100%',
     backgroundImage: `url(${HomeAsiaBackgroundImage})`,
-}
+    backgroundRepeat: 'no-repeat'
+};
+
+const roadToAsia = {
+    display: 'flex',
+    flex: '1',
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
+const roadToAsiaTitle = {
+    textAlign: 'center'
+};
 
 export default HomeAsia;
